fbc96dff9062528780f26fc449d422717108effb
Filipa Barros <barrosfilipa.f@gmail.com>
2023-06-14 20:19:36 +0200

    startScreen

---
+++ button.js
@@ -0,0 +1,45 @@
+class Button {
+  constructor(x, y, width, height, text, onClick) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.text = text;
+    this.onClick = onClick;
+  }
+
+  draw() {
+    const buttonColor = "#333131";
+    const textColor = "white";
+    const buttonPadding = 10;
+    const strokeWidth = 2;
+    const strokeColor = "#262525";
+
+    ctx.fillStyle = buttonColor;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    ctx.lineWidth = strokeWidth;
+    ctx.strokeStyle = strokeColor;
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+
+    ctx.fillStyle = textColor;
+    ctx.font = "24px Rubik";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(
+      this.text,
+      this.x + this.width / 2,
+      this.y + this.height / 2,
+      this.width - buttonPadding * 2
+    );
+  }
+
+  isClicked(x, y) {
+    return (
+      x > this.x &&
+      x < this.x + this.width &&
+      y > this.y &&
+      y < this.y + this.height
+    );
+  }
+}
--- index.js
+++ index.js
@@ -4,6 +4,102 @@ const ctx = canvas.getContext("2d");
 canvas.width = 900;
 canvas.height = 600;
 
+const imgBackground = new Image();
+imgBackground.src = "./imgs/lead_article.jpg";
+
+canvas.addEventListener("click", startGame);
+
+//button config
+const buttons = [];
+const startButton = new Button(350, 300, 200, 55, "Start Game", startGame);
+buttons.push(startButton);
+
+canvas.addEventListener("click", handleButtonClick);
+
+function handleButtonClick(event) {
+  const rect = canvas.getBoundingClientRect();
+  const mouseX = event.clientX - rect.left;
+  const mouseY = event.clientY - rect.top;
+
+  // Check each button for a click
+  buttons.forEach((button) => {
+    if (button.isClicked(mouseX, mouseY)) {
+      button.onClick();
+    }
+  });
+}
+
+const introAudio = new Audio();
+introAudio.src = "/audios/burning-126066.mp3";
+
+function playIntro() {
+  introAudio.play();
+}
+
+// StartScreen
+function drawStartScreen() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // background
+  ctx.fillStyle = "rgba(20, 20, 20, 0.2)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.drawImage(imgBackground, 0, 0, canvas.width, canvas.height);
+
+  // Title
+  ctx.fillStyle = "#d9809e";
+  ctx.font = "72px Rubik";
+  ctx.textAlign = "center";
+
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 4;
+  ctx.strokeText("BERGHAIN DEFENSE", canvas.width / 2, 250);
+
+  ctx.fillText("BERGHAIN DEFENSE", canvas.width / 2, 250);
+
+  // Add buttons
+  buttons.forEach((button) => {
+    button.draw();
+  });
+
+  //and instructions
+  const instructionsColor = "white";
+  const instructionsFont = "18px Rubik";
+  const instructionsX = canvas.width / 2;
+  const instructionsY = canvas.height / 2 + 100;
+  const instructionLines = [
+    "Instructions:",
+    `You are Sven, and your mission is to keep all the people with the wrong look and wrong vibe`,
+    `from entering Berghain, Berlin's most inclusive club. You're Berghain's only hope. `,
+    `Build Svens to throw pills at the incoming crowd to keep them from entering and ruining Berghain.`,
+  ];
+
+  const rectWidth = 850;
+  const rectHeight = instructionLines.length * 30 + 20; // Adjust the height based on line spacing and number of lines
+  const rectX = instructionsX - rectWidth / 2;
+  const rectY = instructionsY - 20;
+
+  ctx.fillStyle = "rgba(0, 0, 0, 0.7)"; // Adjust the color and transparency as needed
+  ctx.fillRect(rectX, rectY, rectWidth, rectHeight);
+
+  ctx.fillStyle = instructionsColor;
+  ctx.font = instructionsFont;
+  for (let i = 0; i < instructionLines.length; i++) {
+    const lineY = instructionsY + i * 30;
+    ctx.fillText(instructionLines[i], instructionsX, lineY);
+  }
+
+  playIntro();
+}
+//
+//
+imgBackground.onload = function () {
+  drawStartScreen();
+};
+//////
+///
+///
+///
 // Global Variables
 const cellSize = 100;
 const cellGap = 3;
@@ -12,22 +108,6 @@ let money = 300;
 let score = 0;
 let frame = 0;
 let chosenBouncer = 1;
-
-// mouse
-const mouse = {
-  x: undefined,
-  y: undefined,
-  width: 0.1,
-  height: 0.1,
-  clicked: false,
-};
-canvas.addEventListener("mousedown", function () {
-  mouse.clicked = true;
-});
-
-canvas.addEventListener("mouseup", function () {
-  mouse.clicked = false;
-});
 
 // Images
 
@@ -52,20 +132,17 @@ imgSide4.src = "./imgs/side4.png";
 const backgroundImg = new Image();
 backgroundImg.src = "./imgs/pixil-frame-0 (7).png";
 
-// const imgHorizontal =
+const imgSven2 = new Image();
+imgSven2.src = "./imgs/sven.png";
 
-let canvasPosition = canvas.getBoundingClientRect();
-canvas.addEventListener("mousemove", function (e) {
-  mouse.x = e.x - canvasPosition.left;
-  mouse.y = e.y - canvasPosition.top;
-});
-
-canvas.addEventListener("mouseleave", function (e) {
-  mouse.x = undefined;
-  mouse.y = undefined;
-});
-
-// Game Board
+// mouse
+const mouse = {
+  x: undefined,
+  y: undefined,
+  width: 0.1,
+  height: 0.1,
+  clicked: false,
+};
 
 // Control Bar
 const controlsBar = {
@@ -84,65 +161,6 @@ const waypoints = [0, 2, 4, 8, 10, 14, 16, 18, 20];
 // console.log(gameGrid);
 const path = [];
 
-// console.log("asdadsasd");
-
-// Collision
-function collision(first, second) {
-  if (
-    first.x < second.x + second.width &&
-    first.x + first.width > second.x &&
-    first.y < second.y + second.height &&
-    first.y + first.height > second.y
-  ) {
-    return true; // Collision detected
-  }
-  return false; // No collision
-}
-
-// GRID
-
-function createGrid() {
-  for (let y = cellSize; y < canvas.height; y += cellSize) {
-    for (let x = 0; x < canvas.width; x += cellSize) {
-      let cell = new Cell(x, y);
-      gameGrid.push(cell);
-      let gameGridIndex = gameGrid.length - 1;
-      if (pathIndexes.includes(gameGridIndex)) {
-        path[pathIndexes.indexOf(gameGridIndex)] = cell;
-      }
-    }
-  }
-}
-
-createGrid();
-// console.log(gameGrid);
-function handleGameGrid() {
-  for (let i = 0; i < gameGrid.length; i++) {
-    gameGrid[i].draw();
-    gameGrid[i].fill(backgroundImg);
-    for (let i = 0; i < path.length; i++) {
-      if (i === 0 || i === 10) {
-        path[i].fill(imgSide3);
-      } else if (i === 2 || i === 14) {
-        path[i].fill(imgSide2);
-      } else if (i === 8 || i === 18) {
-        path[i].fill(imgSide1);
-      } else if ((i === 4) | (i === 16)) {
-        path[i].fill(imgSide4);
-      } else if (i === 3 || i === 9 || i === 15 || i === 19) {
-        path[i].fill(imgHorizontal);
-      } else {
-        path[i].fill(imgVertical);
-      }
-    }
-  }
-}
-
-////
-///
-// console.log(path);
-
-//
 // Base - Berghain
 const base = new Base();
 
@@ -151,10 +169,8 @@ function handleBase() {
   return base.draw();
 }
 
-//
-//
-//
-// BOUNCERS (defenders)
+/// Bouncers
+
 const bouncers = [];
 
 const bouncer1 = {
@@ -163,116 +179,8 @@ const bouncer1 = {
   width: 70,
   height: 70,
 };
-
-const bouncer2 = {
-  x: 600,
-  y: 20,
-  width: 70,
-  height: 70,
-};
-
-const imgSven2 = new Image();
-imgSven2.src = "./imgs/sven.png";
-
-function chooseBouncer() {
-  let bouncer1Stroke = "black";
-  if (collision(mouse, bouncer1) && mouse.clicked === true) {
-    chosenBouncer = 1;
-  }
-
-  if (chosenBouncer === 1) {
-    bouncer1Stroke = "gold";
-  } else {
-    bouncer1Stroke = "black";
-  }
-
-  ctx.lineWidth = 1;
-  ctx.fillStyle = "rgba(0,0,0,0.2)";
-  ctx.fillRect(bouncer1.x, bouncer1.y, bouncer1.width, bouncer1.height);
-  ctx.strokeStyle = bouncer1Stroke;
-  ctx.strokeRect(bouncer1.x, bouncer1.y, bouncer1.width, bouncer1.height);
-  ctx.drawImage(
-    imgSven2,
-    bouncer1.x,
-    bouncer1.y,
-    bouncer1.width,
-    bouncer1.height
-  );
-  // ctx.fillRect(bouncer2.x, bouncer2.y, bouncer2.width, bouncer2.height);
-}
-
-// ADD BOUNCER FUNCTION
-canvas.addEventListener("click", function () {
-  let bouncerCost = 100;
-
-  const gridPositionX = mouse.x - (mouse.x % cellSize);
-  const gridPositionY = mouse.y - (mouse.y % cellSize);
-  let isClashed;
-
-  // not allowing to place multiple bouncers in the same place
-  for (let i = 0; i < bouncers.length; i++) {
-    if (bouncers[i].x === gridPositionX && bouncers[i].y === gridPositionY) {
-      return;
-    }
-  }
-
-  // Check if click is in the path
-  for (let i = 0; i < path.length; i++) {
-    const element = path[i];
-    isClashed = element.x === gridPositionX && element.y === gridPositionY;
-    if (isClashed) {
-      return true;
-    }
-  }
-  if (isClashed === false && money >= bouncerCost) {
-    bouncers.push(new Bouncer(gridPositionX, gridPositionY));
-    money -= bouncerCost;
-  }
-});
-function handleBouncers() {
-  bouncers.forEach((bouncer) => {
-    bouncer.update();
-    bouncer.target = null;
-
-    const validEnemies = enemies.filter((enemy) => {
-      const xDistance = enemy.x - bouncer.x; // adjust to be in center
-      const yDistance = enemy.y - bouncer.y;
-      const distance = Math.hypot(xDistance, yDistance);
-      return distance < enemy.radius + bouncer.radius;
-    });
-
-    bouncer.target = validEnemies[0];
-
-    for (let i = bouncer.projectiles.length - 1; i >= 0; i--) {
-      const projectile = bouncer.projectiles[i];
-
-      projectile.update();
-
-      const xDistance = projectile.enemy.x - projectile.x; // adjust to be in center
-      const yDistance = projectile.enemy.y - projectile.y;
-      const distance = Math.hypot(xDistance, yDistance);
-
-      // when projectile hits enemy
-      if (distance < projectile.enemy.radius + projectile.radius) {
-        projectile.enemy.health -= 20;
-        if (projectile.enemy.health <= 0) {
-          const enemyIndex = enemies.findIndex((enemy) => {
-            return projectile.enemy === enemy;
-          });
-          if (enemyIndex > -1) enemies.splice(enemyIndex, 1); // avoid bugs with removal from array
-          money += 20;
-          score += 50;
-        }
-
-        // console.log(projectile.enemy.health);
-        bouncer.projectiles.splice(i, 1);
-      }
-    }
-  });
-}
-///////////// enemies ///////////////
 //
-//
+/// ENEMIES
 //
 const enemyTypes = [];
 const enemy1 = new Image();
@@ -294,91 +202,288 @@ const enemies = [];
 let enemyCount = 5;
 let wave = 1;
 
-function spawnEnemies(spawnCount) {
-  let offset = 100;
-  for (let i = 1; i < spawnCount; i++) {
-    // setInterval(
-    enemies.push(
-      new Enemy(path[0].getXWaypoint() - offset * i, path[0].getYWaypoint())
-    );
-    // 1000
-    // );
+// Collision
+function collision(first, second) {
+  if (
+    first.x < second.x + second.width &&
+    first.x + first.width > second.x &&
+    first.y < second.y + second.height &&
+    first.y + first.height > second.y
+  ) {
+    return true; // Collision detected
   }
+  return false; // No collision
 }
-spawnEnemies(enemyCount);
 
-let health = base.health;
+////////////////////////////////////////////////////
 
-function handleEnemies() {
-  for (let i = enemies.length - 1; i >= 0; i--) {
-    const enemy = enemies[i];
-    enemy.update();
-  }
+function startGame() {
+  canvas.removeEventListener("click", startGame);
+  canvas.removeEventListener("click", handleButtonClick);
 
-  enemies.forEach((enemy) => {
-    if (enemy.x + enemy.radius === base.x) {
-      health -= enemy.damage;
-      enemies.splice(0, 1);
-    }
-
-    if (health === 0) {
-      console.log("game over");
-    }
+  canvas.addEventListener("mousedown", function () {
+    mouse.clicked = true;
   });
 
-  // tracking total amount of enemies
-  if (enemies.length === 0) {
-    enemyCount += 4;
-    spawnEnemies(enemyCount);
-    wave++;
+  canvas.addEventListener("mouseup", function () {
+    mouse.clicked = false;
+  });
+
+  // const imgHorizontal =
+
+  let canvasPosition = canvas.getBoundingClientRect();
+  canvas.addEventListener("mousemove", function (e) {
+    mouse.x = e.x - canvasPosition.left;
+    mouse.y = e.y - canvasPosition.top;
+  });
+
+  canvas.addEventListener("mouseleave", function (e) {
+    mouse.x = undefined;
+    mouse.y = undefined;
+  });
+
+  // Game Board
+
+  // console.log("asdadsasd");
+
+  // GRID
+
+  function createGrid() {
+    for (let y = cellSize; y < canvas.height; y += cellSize) {
+      for (let x = 0; x < canvas.width; x += cellSize) {
+        let cell = new Cell(x, y);
+        gameGrid.push(cell);
+        let gameGridIndex = gameGrid.length - 1;
+        if (pathIndexes.includes(gameGridIndex)) {
+          path[pathIndexes.indexOf(gameGridIndex)] = cell;
+        }
+      }
+    }
   }
-}
 
-// resources
-function handleGameStatus() {
-  // Money
-  ctx.fillStyle = "gold";
-  ctx.font = "15px Petch";
-  ctx.fillText("Money: " + money, 30, 55);
-
-  // Base Health
-  ctx.fillStyle = "gold";
-  ctx.font = "15px Petch";
-  ctx.fillText("Coolness Level: " + health, 720, 55);
-
-  // Wave counter
-  ctx.fillStyle = "gold";
-  ctx.font = "15px Petch";
-  ctx.fillText("Wave: " + wave, 170, 55);
-
-  // Score
-  ctx.fillStyle = "gold";
-  ctx.font = "15px Petch";
-  ctx.fillText("Score: " + score, 300, 55);
-}
-
-// utilities
-
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "rgb(56,56,56)";
-  ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
-  handleGameGrid();
-  handleBouncers();
-  handleGameStatus();
-  handleBase();
-  chooseBouncer();
-  handleEnemies();
-
-  frame++;
-
-  const animationId = requestAnimationFrame(animate); // creates animation loop
-
-  if (health === 0) {
-    cancelAnimationFrame(animationId);
-    document.querySelector("#game-over").style.display = "flex";
+  createGrid();
+  // console.log(gameGrid);
+  function handleGameGrid() {
+    for (let i = 0; i < gameGrid.length; i++) {
+      gameGrid[i].draw();
+      gameGrid[i].fill(backgroundImg);
+      for (let i = 0; i < path.length; i++) {
+        if (i === 0 || i === 10) {
+          path[i].fill(imgSide3);
+        } else if (i === 2 || i === 14) {
+          path[i].fill(imgSide2);
+        } else if (i === 8 || i === 18) {
+          path[i].fill(imgSide1);
+        } else if ((i === 4) | (i === 16)) {
+          path[i].fill(imgSide4);
+        } else if (i === 3 || i === 9 || i === 15 || i === 19) {
+          path[i].fill(imgHorizontal);
+        } else {
+          path[i].fill(imgVertical);
+        }
+      }
+    }
   }
-}
-// console.log(path);
 
-animate();
+  ////
+  ///
+  //
+  //
+  // BOUNCERS (defenders)
+
+  const bouncer2 = {
+    x: 600,
+    y: 20,
+    width: 70,
+    height: 70,
+  };
+
+  function chooseBouncer() {
+    let bouncer1Stroke = "black";
+    if (collision(mouse, bouncer1) && mouse.clicked === true) {
+      chosenBouncer = 1;
+    }
+
+    if (chosenBouncer === 1) {
+      bouncer1Stroke = "gold";
+    } else {
+      bouncer1Stroke = "black";
+    }
+
+    ctx.lineWidth = 1;
+    ctx.fillStyle = "rgba(0,0,0,0.2)";
+    ctx.fillRect(bouncer1.x, bouncer1.y, bouncer1.width, bouncer1.height);
+    ctx.strokeStyle = bouncer1Stroke;
+    ctx.strokeRect(bouncer1.x, bouncer1.y, bouncer1.width, bouncer1.height);
+    ctx.drawImage(
+      imgSven2,
+      bouncer1.x,
+      bouncer1.y,
+      bouncer1.width,
+      bouncer1.height
+    );
+    // ctx.fillRect(bouncer2.x, bouncer2.y, bouncer2.width, bouncer2.height);
+  }
+
+  // ADD BOUNCER FUNCTION
+  canvas.addEventListener("click", function () {
+    let bouncerCost = 100;
+
+    const gridPositionX = mouse.x - (mouse.x % cellSize);
+    const gridPositionY = mouse.y - (mouse.y % cellSize);
+    let isClashed;
+
+    // not allowing to place multiple bouncers in the same place
+    for (let i = 0; i < bouncers.length; i++) {
+      if (bouncers[i].x === gridPositionX && bouncers[i].y === gridPositionY) {
+        return;
+      }
+    }
+
+    // Check if click is in the path
+    for (let i = 0; i < path.length; i++) {
+      const element = path[i];
+      isClashed = element.x === gridPositionX && element.y === gridPositionY;
+      if (isClashed) {
+        return true;
+      }
+    }
+    if (isClashed === false && money >= bouncerCost) {
+      bouncers.push(new Bouncer(gridPositionX, gridPositionY));
+      money -= bouncerCost;
+    }
+  });
+  function handleBouncers() {
+    bouncers.forEach((bouncer) => {
+      bouncer.update();
+      bouncer.target = null;
+
+      const validEnemies = enemies.filter((enemy) => {
+        const xDistance = enemy.x - bouncer.x; // adjust to be in center
+        const yDistance = enemy.y - bouncer.y;
+        const distance = Math.hypot(xDistance, yDistance);
+        return distance < enemy.radius + bouncer.radius;
+      });
+
+      bouncer.target = validEnemies[0];
+
+      for (let i = bouncer.projectiles.length - 1; i >= 0; i--) {
+        const projectile = bouncer.projectiles[i];
+
+        projectile.update();
+
+        const xDistance = projectile.enemy.x - projectile.x; // adjust to be in center
+        const yDistance = projectile.enemy.y - projectile.y;
+        const distance = Math.hypot(xDistance, yDistance);
+
+        // when projectile hits enemy
+        if (distance < projectile.enemy.radius + projectile.radius) {
+          projectile.enemy.health -= 20;
+          if (projectile.enemy.health <= 0) {
+            const enemyIndex = enemies.findIndex((enemy) => {
+              return projectile.enemy === enemy;
+            });
+            if (enemyIndex > -1) enemies.splice(enemyIndex, 1); // avoid bugs with removal from array
+            money += 20;
+            score += 50;
+          }
+
+          // console.log(projectile.enemy.health);
+          bouncer.projectiles.splice(i, 1);
+        }
+      }
+    });
+  }
+  ///////////// enemies ///////////////
+  //
+  //
+
+  function spawnEnemies(spawnCount) {
+    let offset = 100;
+    for (let i = 1; i < spawnCount; i++) {
+      // setInterval(
+      enemies.push(
+        new Enemy(path[0].getXWaypoint() - offset * i, path[0].getYWaypoint())
+      );
+      // 1000
+      // );
+    }
+  }
+  spawnEnemies(enemyCount);
+
+  let health = base.health;
+
+  function handleEnemies() {
+    for (let i = enemies.length - 1; i >= 0; i--) {
+      const enemy = enemies[i];
+      enemy.update();
+    }
+
+    enemies.forEach((enemy) => {
+      if (enemy.x + enemy.radius === base.x) {
+        health -= enemy.damage;
+        enemies.splice(0, 1);
+      }
+
+      if (health === 0) {
+        console.log("game over");
+      }
+    });
+
+    // tracking total amount of enemies
+    if (enemies.length === 0) {
+      enemyCount += 4;
+      spawnEnemies(enemyCount);
+      wave++;
+    }
+  }
+
+  // resources
+  function handleGameStatus() {
+    // Money
+    ctx.fillStyle = "gold";
+    ctx.font = "15px Rubik";
+    ctx.fillText("Money: " + money, 30, 55);
+
+    // Base Health
+    ctx.fillStyle = "gold";
+    ctx.font = "15px Rubik";
+    ctx.fillText("Coolness Level: " + health, 720, 55);
+
+    // Wave counter
+    ctx.fillStyle = "gold";
+    ctx.font = "15px Rubik";
+    ctx.fillText("Wave: " + wave, 170, 55);
+
+    // Score
+    ctx.fillStyle = "gold";
+    ctx.font = "15px Rubik";
+    ctx.fillText("Score: " + score, 300, 55);
+  }
+
+  // utilities
+
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgb(56,56,56)";
+    ctx.fillRect(0, 0, controlsBar.width, controlsBar.height);
+    handleGameGrid();
+    handleBouncers();
+    handleGameStatus();
+    handleBase();
+    chooseBouncer();
+    handleEnemies();
+
+    frame++;
+
+    const animationId = requestAnimationFrame(animate); // creates animation loop
+
+    if (health === 0) {
+      cancelAnimationFrame(animationId);
+      document.querySelector("#game-over").style.display = "flex";
+    }
+  }
+  // console.log(path);
+
+  animate();
+}
